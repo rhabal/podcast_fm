02614d418be5a5d0efededace11e1298e6c68fff
Rami Habal <rhabal@gmail.com>
2015-09-28 01:38:34 -0700

    added mp3 upload to episodes

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -4,5 +4,9 @@ class Episode < ActiveRecord::Base
   has_attached_file :episode_thumbnail, :styles => { :large => "1000x1000#", :medium => "550x550#" }
   validates_attachment_content_type :episode_thumbnail, :content_type => /\Aimage\/.*\Z/
 
+  has_attached_file :mp3
+  validates_attachment :mp3,
+    :content_type => { :content_type => ["audio/mp3", "audio/mpeg"] },
+    :file_name => { :matches => [/mp3\Z/] }
 
 end
