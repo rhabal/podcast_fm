d025bf2aad599122125fc06a4557ed9b67e06675
Rami Habal <rhabal@gmail.com>
2015-09-28 00:25:41 -0700

    Added paperclip images and updated models/views/controllers

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise', '~> 3.5.2'
+gem 'paperclip', '~> 4.3.1'
 
 group :development, :test do
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,11 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
   	devise_parameter_sanitizer.for(:sign_up) { |u| 
-  		u.permit(:email, :password, :password_confirmation, :title)
+  		u.permit(:email, :password, :password_confirmation, :title, :thumbnail)
   	 }  	
   	 
   	devise_parameter_sanitizer.for(:account_update) { |u| 
-  		u.permit(:email, :password_confirmation, :current_password, :title, :description, :itunes, :stitcher, :podbay)
+  		u.permit(:email, :password_confirmation, :current_password, :thumbnail, :title, :description, :itunes, :stitcher, :podbay)
   	 }
   end
 
